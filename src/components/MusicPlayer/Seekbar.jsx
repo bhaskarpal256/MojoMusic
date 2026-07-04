d12d7bb5aa954b082e7efd51cd254cf5bc767ecd
harmeetpal258 <123814187+harmeetpal258@users.noreply.github.com>
2026-07-05 00:21:20 +0530

    fix: display seekbar in mobile views and adjust layout width

--- src/components/MusicPlayer/Seekbar.jsx
+++ src/components/MusicPlayer/Seekbar.jsx
@@ -5,11 +5,11 @@ const Seekbar = ({ value, min, max, onInput, setSeekTime, appTime }) => {
   const getTime = (time) => `${Math.floor(time / 60)}:${(`0${Math.floor(time % 60)}`).slice(-2)}`;
 
   return (
-    <div className="hidden sm:flex flex-row items-center">
+    <div className="flex flex-row items-center">
       <button type="button" onClick={() => setSeekTime(appTime - 5)} className="hidden lg:mr-4 lg:block text-white">
         -
       </button>
-      <p className="text-white">{value === 0 ? '0:00' : getTime(value)}</p>
+      <p className="text-white text-xs sm:text-sm">{value === 0 ? '0:00' : getTime(value)}</p>
       <input
         type="range"
         step="any"
@@ -17,9 +17,9 @@ const Seekbar = ({ value, min, max, onInput, setSeekTime, appTime }) => {
         min={min}
         max={max}
         onInput={onInput}
-        className="md:block w-24 md:w-56 2xl:w-96 h-1 mx-4 2xl:mx-6 rounded-lg"
+        className="w-20 sm:w-56 2xl:w-96 h-1 mx-2 sm:mx-4 rounded-lg cursor-pointer accent-white"
       />
-      <p className="text-white">{max === 0 ? '0:00' : getTime(max)}</p>
+      <p className="text-white text-xs sm:text-sm">{max === 0 ? '0:00' : getTime(max)}</p>
       <button type="button" onClick={() => setSeekTime(appTime + 5)} className="hidden lg:ml-4 lg:block text-white">
         +
       </button>
